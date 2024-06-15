cf70891dec9616b6c91135b342d93f75df6f4478
ICDM2024 <hzhang15@syr.edu>
2024-06-14 23:28:28 -0400

    Update test_unfairness.py

--- test_unfairness.py
+++ test_unfairness.py
@@ -4,14 +4,12 @@ import networkx as nx
 import community
 import numpy as np
 
-Name = "metis_output/princeton_sequential"
+Name = "princeton_sequential"
 
 ins = open(Name, "r" )
 data = []
-
 number_of_community = 0
 authors = []
-count = 1
 for line in ins:
     authors.append(count)
     count+=1
@@ -20,11 +18,5 @@ for line in ins:
     if (numbers[0]>number_of_community):
         number_of_community = numbers[0]
     data.append(numbers[0]) # Add the "row" to your list.
-parallel_community = []
-for i in range(0,number_of_community+1):
-    parallel_community.append([])
-print(parallel_community)
-print(len(data))
-for i in range(0,len(data)):
-    #print(data[i])
-    parallel_community[data[i]].append(authors[i])
+
+...
